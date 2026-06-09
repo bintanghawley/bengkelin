871455906504b2d727f6828966d431b06f4b010c
bintanghawley <kanaiakasa@gmail.com>
2026-06-09 12:14:15 +0700

    membuat api pada tabel lainnya (servis, ban, oli, dan sparepart

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ServiceController;
+use App\Http\Controllers\Api\TireController;
+use App\Http\Controllers\Api\OilController;
+use App\Http\Controllers\Api\SparepartController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
+Route::apiResource('services', ServiceController::class);
+Route::apiResource('tires', TireController::class);
+Route::apiResource('oils', OilController::class);
+Route::apiResource('spareparts', SparepartController::class);
+
